e4f23aeefd671a9479bc58639bdeec5bf03823bc
SATYAM MISHRA <satyam.mishra975@gmail.com>
2021-11-15 05:23:40 +0000

    add gitpod config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,21 @@
+import WindiCSS from 'vite-plugin-windicss';
+import build from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: build(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			plugins: [
+				WindiCSS()
+			]
+		}
 	}
 };
 
