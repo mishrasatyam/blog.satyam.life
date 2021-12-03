d82092fce1354d05f733a93038e66d51bf042e84
satyam mishra <satyam.mishra975@gmail.com>
2021-12-03 11:55:39 +0530

    check browser true for localstorage

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,7 @@
     import Sun from "$lib/icons/Sun.svelte";
     import Moon from '$lib/icons/Moon.svelte';    
     import {browser} from '$app/env'
+
     if(browser){
         console.log($color_mode)
         if($color_mode=='dark'){
@@ -18,11 +19,11 @@
         <header class="font-semibold text-2xl">Satyam's blog</header>
     </a>
     {#if $color_mode =='light'}
-        <div on:click={()=>{$color_mode='dark';localStorage.setItem('color_mode','dark');document.body.classList.add('black')}}>
+        <div class="cursor-pointer" on:click={()=>{$color_mode='dark';localStorage.setItem('color_mode','dark');document.body.classList.add('black')}}>
             <Sun size="2rem"/>
         </div>
     {:else}
-        <div on:click={()=>{$color_mode='light';localStorage.setItem('color_mode','light');document.body.classList.remove('black')}}>
+        <div class="cursor-pointer" on:click={()=>{$color_mode='light';localStorage.setItem('color_mode','light');document.body.classList.remove('black')}}>
             <Moon size="2rem"/>
         </div>
     {/if}
