83d58848027de5b7b7e7401b962751a070a39899
satyam mishra <satyam.mishra975@gmail.com>
2021-12-03 11:59:41 +0530

    check browser true for localstorage

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,3 @@
 import {browser} from '$app/env'
 import { writable } from 'svelte/store';
-export const color_mode = writable(browser?localStorage.getItem('color_mode'):'dark')
+export const color_mode = writable(browser?localStorage.getItem('color_mode'):'light')
