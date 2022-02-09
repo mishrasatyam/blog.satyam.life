8d0f471d1261a3aef2ff74b732f3d3635f413f76
satyam mishra <satyam.mishra975@gmail.com>
2022-02-09 17:27:54 +0530

    update deps

--- svelte.config.js
+++ svelte.config.js
@@ -8,13 +8,7 @@ const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
 
   kit: {
-    // By default, `npm run build` will create a standard Node app.
-    // You can create optimized builds for different platforms by
-    // specifying a different adapter
-    adapter: build(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
+    adapter: build()
   },
 
   preprocess: [
